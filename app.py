f4935a9cc32d14a75a4cd210ecd9f75e1d03dc1b
cyberandy <andrea@insideout.io>
2023-06-11 20:30:17 +0200

    adding image comparison and captioning

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from jina import DocumentArray, Document
 from PIL import Image
 from io import BytesIO
 import os
+import numpy as np
+from streamlit_image_comparison import image_comparison
 
 
 def get_file_size(image_bytes):
-    return len(image_bytes) / 1024  # Convert to KB
+    return len(image_bytes.getvalue()) / 1024  # Convert to KB
 
 
 def get_image_size(image):
@@ -18,67 +20,92 @@ def get_image_size(image):
 
 def upscale_image(uri, model, scale):
     doc = Document(
-        uri=uri, tags={"image_format": "png", "output_path": "upscaled_image.png"}
+        uri=uri, tags={"image_format": "png", "output_path": "upscaled_image.jpg"}
     )
     docs = DocumentArray([doc])
-
     result = model.upscale(docs=docs, scale=scale)
     return result
 
 
+def generate_caption(client, image):
+    caption = client.caption(image=image)
+    return caption
+
+
 def main():
     st.sidebar.image("assets/logo-wordlift.png", width=200)
-
-    st.sidebar.title("Image Upscaling Settings")
+    st.sidebar.title("Image Upscaling and Captioning Settings")
     access_token = st.sidebar.text_input("Access Token")
-    model_name = st.sidebar.text_input("Model Name", value="LapSRN_x2")
+    upscale_model_name = st.sidebar.text_input("Upscale Model Name", value="LapSRN_x2")
+    caption_model_name = st.sidebar.text_input(
+        "Caption Model Name", value="Salesforce/blip2-flan-t5-xl"
+    )
     width = st.sidebar.number_input("Image Width", value=1200)
     height = st.sidebar.number_input("Image Height", value=-1)
 
-    st.title("Image Upscaling App")
+    st.title("Image Upscaling and Captioning 🖼️")
 
     image_uri = st.text_input("Image URL")
 
-    if st.button("Upscale"):
-        if not access_token or not model_name or not image_uri:
-            st.warning("Please enter Access Token, Model Name, and Image URL")
-        else:
-            st.info("Upscaling in progress...")
-            client = Client(token=access_token)
-            model = client.get_model(model_name)
-            result = upscale_image(image_uri, model, f"{width}:{height}")
+    if access_token and upscale_model_name and image_uri and st.button("Upscale"):
+        st.info("Upscaling in progress...")
+        client = Client(token=access_token)
+        upscale_model = client.get_model(upscale_model_name)
+        result = upscale_image(image_uri, upscale_model, f"{width}:{height}")
 
-            original_image = Image.open(BytesIO(requests.get(image_uri).content))
-            original_weight = get_file_size(requests.get(image_uri).content)
-            original_width, original_height = get_image_size(original_image)
+        original_image = Image.open(BytesIO(requests.get(image_uri).content))
+        original_weight = get_file_size(BytesIO(requests.get(image_uri).content))
+        original_width, original_height = get_image_size(original_image)
 
-            for r in result:
-                upscaled_image_bytes = r.blob
-                upscaled_image = Image.open(BytesIO(upscaled_image_bytes))
+        for r in result:
+            upscaled_image_bytes = r.blob
+            upscaled_image = Image.open(BytesIO(upscaled_image_bytes))
 
-                # Preserve aspect ratio while resizing
-                upscaled_image.thumbnail((width, height), Image.ANTIALIAS)
+            # Preserve aspect ratio while resizing
+            upscaled_image.thumbnail((width, height), Image.ANTIALIAS)
 
-                upscaled_weight = get_file_size(upscaled_image_bytes)
-                upscaled_width, upscaled_height = get_image_size(upscaled_image)
+            # Save the upscaled image in memory in JPEG format for calculating the compressed weight
+            image_bytes = BytesIO()
+            upscaled_image.save(image_bytes, format="JPEG", quality=80)
+            upscaled_weight = get_file_size(image_bytes)
 
-                st.image(
-                    upscaled_image, caption="Upscaled Image", use_column_width=True
-                )
-                st.write("Original Weight: {:.2f} KB".format(original_weight))
-                st.write("Upscaled Weight: {:.2f} KB".format(upscaled_weight))
-                st.write(
-                    "Original Size: {} x {}".format(original_width, original_height)
-                )
-                st.write(
-                    "Upscaled Size: {} x {}".format(upscaled_width, upscaled_height)
-                )
+            upscaled_width, upscaled_height = get_image_size(upscaled_image)
 
-                image_bytes = BytesIO()
-                upscaled_image.save(image_bytes, format="PNG")
-                st.download_button(
-                    "Download", image_bytes.getvalue(), file_name="upscaled_image.png"
-                )
+            # Comparing Original Image and Upscaled Image
+            image_comparison(
+                original_image,
+                upscaled_image,
+                label1="Original Image",
+                label2="Upscaled Image",
+            )
+
+            st.write("Original Image Weight: {:.2f} KB".format(original_weight))
+            st.write(
+                "Upscaled (compressed) Image Weight: {:.2f} KB".format(upscaled_weight)
+            )
+            st.write(
+                "Original Image Size: {} x {}".format(original_width, original_height)
+            )
+            st.write(
+                "Upscaled Image Size: {} x {}".format(upscaled_width, upscaled_height)
+            )
+
+            st.download_button(
+                "Download", image_bytes.getvalue(), file_name="upscaled_image.jpg"
+            )
+
+    if (
+        access_token
+        and caption_model_name
+        and image_uri
+        and st.button("Generate Caption")
+    ):
+        st.info("Generating caption...")
+        client = Client(token=access_token)
+        caption_model = client.get_model(caption_model_name)
+        image_content = requests.get(image_uri).content
+        caption = generate_caption(caption_model, image_content)
+        st.write("Caption: {}".format(caption))
 
 
 if __name__ == "__main__":
